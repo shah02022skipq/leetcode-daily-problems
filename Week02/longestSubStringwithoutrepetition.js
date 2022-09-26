099b210895f1a4fbe8a41e1ee784b527c34c7a6f
shah02022skipq <shah.muhammad.sirius@gmail.com>
2022-09-26 15:08:14 +0500

    adding more dynamic window sliding problems

---
+++ Week02/longestSubStringwithoutrepetition.js
@@ -0,0 +1,23 @@
+
+const logestSubStringWithoutRepition = (str) => {
+    let arr = str.split('')
+
+    let winStart = 0
+    let maxWindowSize = 0
+    let set = []
+    for(let winEnd=0 ; winEnd<arr.length ; winEnd++){
+        
+        while(set.includes(arr[winEnd])){
+            set.shift() //remove from start
+            winStart ++ 
+        }
+        set.push(arr[winEnd])
+        maxWindowSize = Math.max(maxWindowSize, (winEnd - winStart + 1))
+    }
+
+    return maxWindowSize
+}
+
+let str = 'geeksforgeeks'
+
+console.log(logestSubStringWithoutRepition(str))
